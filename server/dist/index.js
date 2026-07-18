f0ca451dff82323a220ac4f1a4953f0eea309bf5
Prasad Kadam <prasadkadam29503@gmail.com>
2026-07-18 18:39:06 +0530

    setup backend express-ts

---
+++ server/dist/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`sever runing on ${PORT}`);
+});
+app.get("/", (req, res) => {
+    res.send("Hiii coinflux");
+});
+//# sourceMappingURL=index.js.map
