d311e4bc69755552b824656b1a225bc8d0c3976f
Prasad Kadam <prasadkadam29503@gmail.com>
2026-07-22 22:30:17 +0530

    routing complete

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,6 +1,10 @@
 import { prisma } from "./lib/prisma.js";
 import express from "express";
+import rootRouter from "./src/index.js";
+import cors from "cors";
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`sever runing on ${PORT}`);
@@ -8,40 +12,5 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
     res.send("Hiii coinflux");
 });
-async function main() {
-    // Create a new user with a post
-    const user = await prisma.user.create({
-        data: {
-            name: "Alice",
-            email: "alice@prisma.io",
-            posts: {
-                create: {
-                    title: "Hello World",
-                    content: "This is my first post!",
-                    published: true,
-                },
-            },
-        },
-        include: {
-            posts: true,
-        },
-    });
-    console.log("Created user:", user);
-    // Fetch all users with their posts
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-        },
-    });
-    console.log("All users:", JSON.stringify(allUsers, null, 2));
-}
-main()
-    .then(async () => {
-    await prisma.$disconnect();
-})
-    .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-});
+app.use("/api/v1", rootRouter);
 //# sourceMappingURL=index.js.map
---
+++ server/dist/src/index.js
@@ -0,0 +1,6 @@
+import express from "express";
+import authRouter from "./auth/auth.js";
+const router = express.Router();
+router.use("/auth", authRouter);
+export default router;
+//# sourceMappingURL=index.js.map
