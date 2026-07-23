fe64449859600dff0625b216d2d6573aafe7a0da
Prasad Kadam <prasadkadam29503@gmail.com>
2026-07-24 00:06:21 +0530

    remove dist

--- server/dist/src/index.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import authRouter from "./auth/auth.js";
-const router = express.Router();
-router.use("/auth", authRouter);
-export default router;
-//# sourceMappingURL=index.js.map
